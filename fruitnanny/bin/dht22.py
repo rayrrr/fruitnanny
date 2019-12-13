dad3c0398609c61107239eaf7f2bae6757b1035e
Ray <7869818+rayrrr@users.noreply.github.com>
2019-12-12 21:37:59 -0500

    dht sensor working with libgpiod2 etc

--- fruitnanny/bin/dht22.py
+++ fruitnanny/bin/dht22.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import sys
 
 import adafruit_dht
 import board
 
 # choose one or the other
-# dht_device = adafruit_dht.DHT22(board.D24)
-dht_device = adafruit_dht.DHT11(board.D24)
+dht = adafruit_dht.DHT22(board.D24)
+# dht = adafruit_dht.DHT11(board.D24)
 
-humidity = dht_device.humidity
-temperature = dht_device.temperature
-if humidity is not None and temperature is not None:
-    print('{0:0.1f} {1:0.1f}'.format(temperature, humidity))
-else:
+for i in range(1,10):
+    try:
+        t = dht.temperature
+        h = dht.humidity
+        if t and h:
+            print('{0:0.1f} {1:0.1f}'.format(t, h))
+            break
+    except:
+        pass
+if not t or not h:
     print('Failed to get reading. Try again!')
     sys.exit(1)
-
